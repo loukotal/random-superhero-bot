879b8b0e67650a89619e13fc167bc22435fd9065
Lukáš Loukota <lukasloukota94@gmail.com>
2018-12-02 20:24:32 +0100

    Post generated names

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from mastodon import Mastodon
 import os
+from textgenrnn import textgenrnn
 
-#
 mastodon = Mastodon(
     access_token=os.getenv("MASTODON_ACCESS_KEY"),
     api_base_url="https://botsin.space"
@@ -9,8 +9,19 @@ mastodon = Mastodon(
 
 
 def lambda_handler(event, context):
-    mastodon.status_post("Hello world from lambda function")
+    #
+    heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
+                              vocab_path="textgenrnn_vocab.json")
+    heroes_list = heroes_names.generate(5, return_as_list=True)
+    heroes = "\n".join(heroes_list)
+    mastodon.status_post("{}".format(heroes))
     return {
         "statusCode": 200,
-        "body": "hello world?"
+        "body": "{}".format(heroes)
     }
+
+
+if __name__ == "__main__":
+    heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
+                              vocab_path="textgenrnn_vocab.json")
+    heroes_names.generate()
