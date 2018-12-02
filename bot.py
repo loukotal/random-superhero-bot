a20ddaca38cd8e23a93307a19357d54ae8c8a116
Lukáš Loukota <lukasloukota94@gmail.com>
2018-12-02 22:40:08 +0100

    Change the logic to get random lines

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from mastodon import Mastodon
 import os
-from textgenrnn import textgenrnn
+import random
+# from textgenrnn import textgenrnn
 
 mastodon = Mastodon(
     access_token=os.getenv("MASTODON_ACCESS_KEY"),
@@ -9,12 +10,15 @@ mastodon = Mastodon(
 
 
 def lambda_handler(event, context):
-    #
-    heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
-                              vocab_path="textgenrnn_vocab.json")
-    heroes_list = heroes_names.generate(5, return_as_list=True)
+    with open("generated_names.txt","r") as f:
+        lines = f.readlines()
+        heroes_list = [random.choice(lines) for _ in range(6)]
+
+    # heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
+    #                           vocab_path="textgenrnn_vocab.json")
+    # heroes_list = heroes_names.generate(5, return_as_list=True)
     heroes = "\n".join(heroes_list)
-    mastodon.status_post("{}".format(heroes))
+    mastodon.status_post("New superheroes on the block!\n\n{}".format(heroes))
     return {
         "statusCode": 200,
         "body": "{}".format(heroes)
@@ -22,6 +26,7 @@ def lambda_handler(event, context):
 
 
 if __name__ == "__main__":
-    heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
-                              vocab_path="textgenrnn_vocab.json")
-    heroes_names.generate()
+    pass
+    # heroes_names = textgenrnn(weights_path="heroes_textgen.hdf5", config_path="textgenrnn_config.json",
+    #                           vocab_path="textgenrnn_vocab.json")
+    # heroes_names.generate_to_file("generated_names.txt", n=10000, temperature=1)
